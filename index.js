5e058b977b84f5d94c49019bfa09178eeb0158e9
TasneemHelmy <fs4191884@gmail.com>
2024-06-28 22:56:27 +0300

    v

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ import userModel from './database/models/user.model.js';
 import postModel from './database/models/post.model.js';
 import commentModel from './database/models/comment.model.js';
 const app=express()
-const port=3000;
+const port=process.env.PORT||3000;
 app.use(cors());
 bootstrap(app,express)
 app.listen(port,(error)=>{
     if(error) console.log(error)
         else console.log("server running");
     });
-    
